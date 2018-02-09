471baf701e36c78306453e7694fd11fd42b31edb
ornst278@nemi.tskoli.is <ornst278@nemi.tskoli.is>
2018-02-09 09:23:53 +0000

    verk5

--- Verk-5/quiz.js
+++ Verk-5/quiz.js
@@ -1,44 +1,59 @@
 var qindex = 0;
 var right = 0;
 var wrong = 0;
-var qs = [
-	"Hitler was born in?",
-	"What is the currency of Japan?"
-];
-var ans = [
-	["Austria", "Germany", "Austria"],
-	["Yen", "Pounds", "Japanese dollars", "Riki", "Yen"]
-];
 
 var rightEl = document.getElementById("score-right");
 var wrongEl = document.getElementById("score-wrong");
 var qEl = document.getElementById("question");
 var ansEl = document.getElementById("ans");
+var ansList;
 
-function setupQ() {
-	rightEl.innerHTML = right;
-	wrongEl.innerHTML = wrong;
-	qEl.innerHTML = qs[qindex];
-	ansEl.innerHTML = "";
+// object definitons
+function Question(q, corr, ans) {
+	this.quest = q;
+	this.corr = corr;
+	this.ans = ans;
 
-	for (var i = 1; i < ans[qindex].length; i++) {
-		ansEl.innerHTML += "<div class=\"ans\">" + ans[qindex][i] + "</div>";
+	this.checkAnswer = function(s) {
+		if (s == corr) {
+			return true;
+		}
+		return false;
+	};
+}
+
+function Game() {
+	this.qindex = 0;
+	this.right = 0;
+	this.wrong = 0;
+
+	this.setupQ = function() {
+		rightEl.innerHTML = right;
+		wrongEl.innerHTML = wrong;
+		qEl.innerHTML = qs[this.qindex].quest;
+		ansEl.innerHTML = "";
+
+		for (var i = 0; i < qs[this.qindex].ans.length; i++) {
+			ansEl.innerHTML += "<div class=\"ans\">" + qs[this.qindex].ans[i] + "</div>";
+		}
+
+		ansList = document.querySelectorAll(".ans");
+	}
+
+	this.nextQ = function() {
+		if (this.qindex + 1 < qs.length) {
+			this.qindex += 1;
+			this.setupQ();
+		}
 	}
 }
 
-setupQ();
+// objects
+var qs = [
+	new Question("Hitler was born in?", "Austria", ["Austria", "Germany"]),
+	new Question("What is the currency of Japan?", "Yen", ["Pounds", "Japanese dollars", "Riki", "Yen"])
+];
+var game = new Game();
 
-var ansList = document.querySelectorAll(".ans");
-
-ansList[0].onclick = checkAns(ansList[0].innerHTML);
-ansList[1].onclick = checkAns(ansList[1].innerHTML);
-
-function checkAns(t) {
-	if (t == ans[qindex][0]) {
-		right++;
-	} else {
-		wrong++;
-	}
-	qindex++;
-	setupQ();
-}
+// start
+game.setupQ();
